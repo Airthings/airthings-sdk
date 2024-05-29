0e7e79823a780f8f9b56d754fcefbdee8d90b0b2
Ståle Storø Hauknes <stale.semb@airthings.com>
2024-05-30 00:35:00 +0200

    Fix example

--- api/python/examples/fetch_devices_and_sensors.py
+++ api/python/examples/fetch_devices_and_sensors.py
@@ -1,10 +1,12 @@
 """Example of fetching devices and sensors from Airthings API."""
 
+import asyncio
 import sys
 
 from airthings_sdk import Airthings
 
-if __name__ == "__main__":
+
+async def main():
     if len(sys.argv) <= 2:
         print("Please add client id and client secret as parameters.")
         print("Usage:")
@@ -13,7 +15,14 @@ if __name__ == "__main__":
     client_id = sys.argv[1]
     client_secret = sys.argv[2]
 
-    airthings = Airthings(client_id=client_id, client_secret=client_secret, is_metric=True)
+    airthings = Airthings(
+        client_id=client_id,
+        client_secret=client_secret,
+        is_metric=True
+    )
 
-    devices = airthings.update_devices()
-    print(airthings.devices)
+    devices = await airthings.update_devices()
+    print(devices)
+
+if __name__ == "__main__":
+    asyncio.run(main())
